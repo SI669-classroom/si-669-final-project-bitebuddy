ff9085f53de6bb81da577a5cae78ae849ebbbaf2
oliviahyw <112413539+oliviahyw@users.noreply.github.com>
2023-11-14 23:08:42 -0500

    Delete BITEBUDDY/Secrets.js

--- BITEBUDDY/Secrets.js
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyA12dkUzYmlWquBe7TWCU7c5CkIYd318rc",
-  authDomain: "bitbuddy-b84ce.firebaseapp.com",
-  projectId: "bitbuddy-b84ce",
-  storageBucket: "bitbuddy-b84ce.appspot.com",
-  messagingSenderId: "6711752826",
-  appId: "1:6711752826:web:d8aa9fca2f6ea953b7e3c3"
-};
